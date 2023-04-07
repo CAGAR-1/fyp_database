00fee556c6a1a12bac32be2b54668090e9ba3464
Sagar <Sagararyal1122@gmail.com>
2023-04-07 21:18:15 +0545

    Add images while Adding Category

--- addCategory.php
+++ addCategory.php
@@ -3,27 +3,54 @@ include 'DatabaseConfig.php';
 include 'helper_functions/authentication_functions.php';
 
 
-    if (isset($_POST['name']) && isset($_POST['description'])) {
+    
+    if (isset($_POST['name']) 
+    && isset($_POST['description']) 
+
+    && isset($_FILES["image"])
+
+   
+    
+    ) {
         $name = $_POST['name'];
         $description = $_POST['description'];
+         //getimage
+         
+        $image = $_FILES['image']['name'];
+        $image_tmp = $_FILES['image']['tmp_name'];
+        $image_size = $_FILES['image']['size'];
+        $image_ext = pathinfo($image, PATHINFO_EXTENSION);
+        $image_path = "images/".$image;
 
-        $addCategory = "INSERT INTO categories (name, description) VALUES ('$name', '$description')";
-        $result = mysqli_query($con, $addCategory);
-        if ($result) { 
-            echo json_encode(
-                [
-                    'success' => true,
-                    'message' => 'Category added'
-                ]
-            );
-        } else {
-            echo json_encode(
-                [
-                    'success' => false,
-                    'message' => 'Error adding category'
-                ]
-            );
-        }
+      //upload image
+      if ($image_size < 5000000) {
+          if ($image_ext == "jpg" || $image_ext == "png" || $image_ext == "jpeg") {
+              if (move_uploaded_file($image_tmp, $image_path)) {
+                  //inserting data into database
+                  $sql = "INSERT INTO categories (name, description,image) VALUES ('$name', '$description','$image_path')";
+                  $query = mysqli_query($con, $sql);
+                  if ($query) {
+                    $data=['success'=>true, 'message'=>'Categories added successfully'];
+                    echo json_encode($data);
+                    //  getProducts("Product added successfully.");
+                  } else {
+                      $data=['success'=>false, 'message'=>'Something went wrong.'];
+                      echo json_encode($data);
+                  }
+              } else {
+                  $data=['success'=>false, 'message'=>'Something went wrong.'];
+                  echo json_encode($data);
+              }
+          } else {
+              $data=['success'=>false, 'message'=>'Image must be jpg, png or jpeg.'];
+              echo json_encode($data);
+          }
+      } else {
+          $data=['success'=>false, 'message'=>'Image size must be less than 5MB.'];
+          echo json_encode($data);
+      }
+
+      
     } else {
         echo json_encode(
             [
@@ -32,3 +59,11 @@ include 'helper_functions/authentication_functions.php';
             ]
         );
     }
+// } else {
+//     echo json_encode(
+//         [
+//             'success' => false,
+//             'message' => 'Access denied'
+//         ]
+//     );
+// }
