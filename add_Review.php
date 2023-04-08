2b9a4db204a1bc5f98557786abb3b3eec29dab86
Sagar <Sagararyal1122@gmail.com>
2023-04-08 11:43:09 +0545

    Add_Review

--- add_Review.php
+++ add_Review.php
@@ -2,12 +2,13 @@
 include 'DatabaseConfig.php';
 
 
-if (isset($_POST['reviews'])&& isset($_POST['shop_id'])&& isset($_POST['user_id']) ) {
+if (isset($_POST['reviews'])&& isset($_POST['shop_id'])&& isset($_POST['user_id'])&& isset($_POST['rating_bar']) ) {
 
     $reviews=$_POST['reviews'];
     $shop_id=$_POST['shop_id'];
     $user_id=$_POST['user_id'];
-    $addReview="INSERT INTO review(reviews,shop_id,user_id)VALUES('$reviews','$shop_id','$user_id')";
+    $rating_bar=$_POST['rating_bar'];
+    $addReview="INSERT INTO review(reviews,shop_id,user_id,rating_bar)VALUES('$reviews','$shop_id','$user_id','$rating_bar')";
     $result=mysqli_query($con,$addReview);
     if ($result) { 
         echo json_encode(
