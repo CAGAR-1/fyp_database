11a9915650f4747a17a9a097d2237848217897df
Sagar <Sagararyal1122@gmail.com>
2023-04-14 20:31:27 +0545

    RESET_PASSWORD

--- reset_password.php
+++ reset_password.php
@@ -1,30 +1,35 @@
 <?php
-include('../../connection.php');
+require_once 'DatabaseConfig.php';
+require_once 'helper_functions/authentication_functions.php';
 
 $otp_code = $_POST["token"];
 $password = md5($_POST["password"]);
 
 // Checking
-$sql = "SELECT * FROM otp_codes WHERE otp=$otp_code";
-$stmt = $conn->prepare($sql);
+$stmt = $con->prepare("SELECT * FROM otp_codes WHERE otp=?");
+$stmt->bind_param("i", $otp_code);
 $stmt->execute();
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-$email = $result['email'];
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+$email = $row['email'];
+
 if ($email) {
-    $sql2 = "UPDATE users SET password='$password' WHERE email='$email'";
-    $stmt2 = $conn->prepare($sql2);
+    $stmt2 = $con->prepare("UPDATE users SET password=? WHERE email=?");
+    $stmt2->bind_param("ss", $password, $email);
     $stmt2->execute();
-    $sql3 = "DELETE FROM otp_codes WHERE otp=$otp_code";
-    $stmt3 = $conn->prepare($sql3);
+
+    $stmt3 = $con->prepare("DELETE FROM otp_codes WHERE otp=?");
+    $stmt3->bind_param("i", $otp_code);
     $stmt3->execute();
 
-    echo (json_encode(array(
+    echo json_encode(array(
         'status' => true,
-        "message" => "Password has been changed",
-    )));
+        'message' => 'Password has been changed',
+    ));
 } else {
-    echo (json_encode(array(
+    echo json_encode(array(
         'status' => false,
-        "message" => "Problem while changing password",
-    )));
+        'message' => 'Problem while changing password',
+    ));
 }
+?>
