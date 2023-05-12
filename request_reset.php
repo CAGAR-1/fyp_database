fe2049e4eefb75f4906abd461d00bc04e61a7017
Sagar <Sagararyal1122@gmail.com>
2023-05-12 13:42:25 +0545

    final

--- request_reset.php
+++ request_reset.php
@@ -47,7 +47,7 @@ else {
     $mail->Host = 'smtp.gmail.com';       // Specify main and backup SMTP servers 
     $mail->SMTPAuth = true;               // Enable SMTP authentication 
     $mail->Username = 'someone24422@gmail.com';   // SMTP username 
-    $mail->Password = '';   // SMTP password 
+    $mail->Password = 'ghj';   // SMTP password 
     $mail->SMTPSecure = 'tls';            // Enable TLS encryption, `ssl` also accepted 
     $mail->Port = 587;                    // TCP port to connect to 
 
